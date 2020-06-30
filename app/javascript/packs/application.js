3c22cf5e1b05180b38bf464e4da98b82eddb6edd
PSzyszka <pawelszyszka26@wp.pl>
2020-06-30 07:37:12 +0200

    Adds new requirements to application.js

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,11 +3,18 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
-require("@rails/ujs").start()
-require("turbolinks").start()
-require("@rails/activestorage").start()
-require("channels")
+// import 'bootstrap';
 
+//= require rails/ujs
+//= require turbolinks
+//= require rails/activestorage
+//= require channels
+//= require jquery
+//= require jquery3
+//= require popper
+//= require bootstrap-sprockets
+//= require bootstrap
+//= require bootstrap-sass/assets/javascripts/bootstrap-sprockets
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
