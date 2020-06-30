2a78d466a916f42013d7874e1b795ee0209a6310
PSzyszka <pawelszyszka26@wp.pl>
2020-06-30 07:54:54 +0200

    Corrects requirements in application.js and .eslintrc.yml

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -5,11 +5,13 @@
 
 // import 'bootstrap';
 
-//= require rails/ujs
-//= require turbolinks
-//= require rails/activestorage
-//= require channels
-//= require jquery
+require('@rails/ujs').start()
+require('turbolinks').start()
+require('@rails/activestorage').start()
+require('channels')
+require('bootstrap')
+require('jquery')
+
 //= require jquery3
 //= require popper
 //= require bootstrap-sprockets
