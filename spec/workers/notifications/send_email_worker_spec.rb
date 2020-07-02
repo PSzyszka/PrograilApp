4e004ea3f936e0e848a3395ae898383070c37802
PSzyszka <pawelszyszka26@wp.pl>
2020-07-02 14:13:30 +0200

    Corrects send_email_worker and it's specs

--- spec/workers/notifications/send_email_worker_spec.rb
+++ spec/workers/notifications/send_email_worker_spec.rb
@@ -6,36 +6,49 @@ Sidekiq::Testing.fake!
 
 RSpec.describe Notifications::SendEmailWorker, type: :worker do
   let!(:user_restaurant)       { create(:user_restaurant) }
-  let!(:subscription)          { create(:subscription, :email_confirmed, user: user_restaurant.user) }
-  let!(:post)                  { create(:post, restaurant: user_restaurant.restaurant) }
-  let(:scheduled_valid_job)   { Notifications::SendEmailWorker.new.perform(post.id) }
-  let(:scheduled_invalid_job) { Notifications::SendEmailWorker.new.perform(post.id + 1) }
+  let(:subscription)           { create(:subscription, user: user_restaurant.user) }
+  let(:confirmed_subscription) { create(:subscription, :email_confirmed, user: user_restaurant.user) }
+  let(:post)                   { create(:post, restaurant: user_restaurant.restaurant) }
+  let(:scheduled_valid_job)    { Notifications::SendEmailWorker.new.perform(post.id) }
+  let(:scheduled_invalid_job)  { Notifications::SendEmailWorker.new.perform(post.id + 1) }
 
   describe '#perform' do
-    context 'when post id is present' do
-      it 'enqueues SendEmailWorker' do
-        expect { Notifications::SendEmailWorker.perform_async }.to change(
-          Notifications::SendEmailWorker.jobs, :size
-        ).by(1)
+    context 'when a subscription is confirmed' do
+      context 'when post id is present' do
+        it 'enqueues SendEmailWorker' do
+          expect { Notifications::SendEmailWorker.perform_async }.to change(
+            Notifications::SendEmailWorker.jobs, :size
+          ).by(1)
+        end
+
+        it 'executes SendUserNotificationMailer' do
+          notification_email = double(:notification_email, deliver_later: nil)
+          mailer = double(:mailer, notification_email: notification_email)
+
+          expect(SendUserNotificationMailer).
+            to receive(:with).
+            with(subscription: confirmed_subscription, post: post).
+            and_return(mailer)
+
+          expect(notification_email).to receive(:deliver_later)
+          scheduled_valid_job
+        end
       end
 
-      it 'executes SendUserNotificationMailer' do
-        notification_email = double(:notification_email, deliver_later: nil)
-        mailer = double(:mailer, notification_email: notification_email)
+      context 'when post id is absent' do
+        it 'raises ActiveRecord::RecordNotFound error' do
+          confirmed_subscription
 
-        expect(SendUserNotificationMailer).
-          to receive(:with).
-          with(subscription: subscription, post: post).
-          and_return(mailer)
-
-        expect(notification_email).to receive(:deliver_later)
-        scheduled_valid_job
+          expect { scheduled_invalid_job }.to raise_error(ActiveRecord::RecordNotFound)
+        end
       end
     end
 
-    context 'when post id is absent' do
-      it 'raises ActiveRecord::RecordNotFound error' do
-        expect { scheduled_invalid_job }.to raise_error(ActiveRecord::RecordNotFound)
+    context 'when no subscription is confirmed' do
+      it 'returns nil' do
+        subscription
+
+        expect(scheduled_valid_job).to be_nil
       end
     end
   end
