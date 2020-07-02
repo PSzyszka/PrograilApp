3ec6fec3733384a60286d65d705d1dcb544262a9
PSzyszka <pawelszyszka26@wp.pl>
2020-07-02 13:45:28 +0200

    Changes facebook_url regexp in model and factory

--- spec/factories/restaurant.rb
+++ spec/factories/restaurant.rb
@@ -3,6 +3,6 @@
 FactoryBot.define do
   factory :restaurant do
     name         { 'Restaurant Name' }
-    facebook_url { 'https://www.facebook.com/pg/bistropiccobello/posts/' }
+    facebook_url { 'https://www.facebook.com/pg/bistropiccobello/posts/?ref=page_internal' }
   end
 end
